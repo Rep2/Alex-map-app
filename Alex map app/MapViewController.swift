7657f5d7ef257767a6999f9eb935a368c7ae8bee
Ivan Rep <ivan.rep2@gmail.com>
2017-08-12 17:41:35 +0200

    MapView

--- Alex map app/MapViewController.swift
+++ Alex map app/MapViewController.swift
@@ -1,6 +1,13 @@
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+
+    lazy var mapView: MKMapView = {
+        let map = MKMapView(frame: .zero)
+
+        return map
+    }()
 
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -8,5 +15,15 @@ class MapViewController: UIViewController {
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupSubvews()
+    }
+
+    func setupSubvews() {
+        
     }
 }
