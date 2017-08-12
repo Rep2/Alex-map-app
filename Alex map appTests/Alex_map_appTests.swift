5b00ccd24eb4de4e366c9392bf57c2a248955c47
Ivan Rep <ivan.rep2@gmail.com>
2017-08-12 13:58:20 +0200

    Initial commit

---
+++ Alex map appTests/Alex_map_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Alex_map_appTests.swift
+//  Alex map appTests
+//
+//  Created by Ivan Rep on 12/08/2017.
+//  Copyright © 2017 Ivan Rep. All rights reserved.
+//
+
+import XCTest
+@testable import Alex_map_app
+
+class Alex_map_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
