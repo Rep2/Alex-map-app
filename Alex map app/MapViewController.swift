e21522e6375e09ab912ca310b26c8ad859fdf4d5
Ivan Rep <ivan.rep2@gmail.com>
2017-08-12 14:05:59 +0200

    Project clean up

---
+++ Alex map app/MapViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MapViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
